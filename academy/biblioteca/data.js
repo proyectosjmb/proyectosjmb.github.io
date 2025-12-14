7c66b504817b75570e191bb68dfac93073bbc40f
Miguel <soybrillantemx@gmail.com>
2025-12-14 00:32:06 -0600

    Academy: biblioteca con categorias, buscador y txtos html

---
+++ academy/biblioteca/data.js
@@ -0,0 +1,31 @@
+// Pega aquí los links públicos reales de Drive en `audioUrl` cuando los tengas.
+// Formato recomendado (archivo público): https://drive.google.com/file/d/<ID>/view?usp=sharing
+const DRIVE_PLACEHOLDER = "https://drive.google.com/file/d/PASTE_ID_HERE/view?usp=sharing";
+
+export const items = [
+  {
+    title: "Agujeros Negros y Antimateria",
+    category: "ciencias",
+    tags: ["astronomía", "física", "ciencias"],
+    date: "2025-12-01",
+    audioUrl: DRIVE_PLACEHOLDER,
+    textUrl: "../textos/agujeros-negros.html",
+  },
+  {
+    title: "Viaje al interior del átomo",
+    category: "ciencias",
+    tags: ["química", "física", "ciencias"],
+    date: "2025-12-01",
+    audioUrl: DRIVE_PLACEHOLDER,
+    textUrl: "../textos/viaje-atomo.html",
+  },
+  {
+    title: "Putrapali y los elefantes de guerra",
+    category: "historia",
+    tags: ["antigüedad", "estrategia", "historia"],
+    date: "2025-12-01",
+    audioUrl: DRIVE_PLACEHOLDER,
+    textUrl: "../textos/putrapali.html",
+  },
+];
+
